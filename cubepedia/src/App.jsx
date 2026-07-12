bfeba307784541f07975a4227da90696c9812813
Srishti Srivastava <srishti.school2010@gmail.com>
2026-07-12 12:50:39 +0530

    Feat: Added the description about the CubePedia and added an image of cube.

--- cubepedia/src/App.jsx
+++ cubepedia/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import CubePedia from "./components/whatscubepedia";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <CubePedia />
     </div>
   );
 };
---
+++ cubepedia/src/components/whatscubepedia.jsx
@@ -0,0 +1,22 @@
+import cube from "../assets/cube.png";
+import "../styles/cubepedia.css";
+
+const CubePedia = () => {
+  return (
+    <div className="cubepedia">
+      <div className="cubepedia-content">
+        <h1>What is CubePedia</h1>
+
+        <p>
+          CubePedia is a digital encyclopedia dedicated to the world of Rubik's
+          Cubes and speedcubing. Explore the history of iconic puzzles, discover
+          the stories behind famous algorithms, learn about world champions, and
+          dive into the hardware that shaped modern cubing—all in one place.
+        </p>
+      </div>
+      <img src={cube} alt="cube" />
+    </div>
+  );
+};
+
+export default CubePedia;
